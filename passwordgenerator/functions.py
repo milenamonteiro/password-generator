4bd0baa40c9fbbfa300b5ab3ca4f66331eabe482
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2019-12-13 20:12:49 -0300

    Changed the random module to secret

--- passwordgenerator/functions.py
+++ passwordgenerator/functions.py
@@ -1,10 +1,10 @@
 '''Modules'''
 import string
-import random
+import secrets # works the same way as random
 
 def new_default(size=12, chars=string.ascii_uppercase +
                 string.ascii_lowercase + string.digits):
     '''
     If the user wants a default password this method is called.
     Returning a 12 size password with letters and numbers'''
-    return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(secrets.choice(chars) for _ in range(size))
