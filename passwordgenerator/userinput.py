cc54a1bfb40af6c56fbad07404250310ab0970a0
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2019-12-13 18:27:43 -0300

    Update and rename passwordgen.py to passwordgenerator/userinput.py

---
+++ passwordgenerator/userinput.py
@@ -0,0 +1,11 @@
+'''Import to get the functions from the other file'''
+import functions as password
+
+"""LENGHT = input('What will be the lenght of the password? ')
+LENGHT = int(LENGHT)
+SYMBOLS = input('Do you want symbols in it? [Y]Yes [N]No ')
+NUMBERS = input('Do you want numbers in it? [Y]Yes [N]No ')
+LOWER = input('Do you want lower case characters in the password? [Y]Yes [N]No ')
+UPPER = input('Do you want upper case characters in the password? [Y]Yes [N]No ')"""
+
+print(password.new_default())
