48f6f577f08f8fb8d7bb7f9ee195a60894f0f2b3
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2019-12-16 00:14:27 -0300

    Funfando

--- passwordgenerator/userinput.py
+++ passwordgenerator/userinput.py
@@ -8,28 +8,25 @@ def yes_no(text):
     _no = set(['no', 'n'])
     while True:
         choice = input(text).lower()
-        # prints the text and attributes the input to choice
         if choice in yes:
             return True
         if choice in _no:
             return False
         print("Please answer with 'Yes' or 'No'")
 
-while True:
-    try:
-        LENGHT = int(input('What will be the lenght of the password? '))
-    except ValueError:
-        print("Please enter an integer number")
-        continue
-    if isinstance(LENGHT, int):
-        # if the input value is integer the looping stops
-        break
-
-# all variables are booleans
-SYMBOLS = yes_no('Do you want symbols in it? [Y]Yes [N]No ')
-NUMBERS = yes_no('Do you want numbers in it? [Y]Yes [N]No ')
-LOWER = yes_no('Do you want lower case characters in the password? [Y]Yes [N]No ')
-UPPER = yes_no('Do you want upper case characters in the password? [Y]Yes [N]No ')
-
-# just testing the function
-print(password.new_default())
+if yes_no('Do you want a default password? [Y]Yes [N]No '):
+    print(password.new_default())
+else:
+    while True:
+        try:
+            LENGHT = int(input('What will be the lenght of the password? '))
+        except ValueError:
+            print("Please enter an integer number")
+            continue
+        if isinstance(LENGHT, int):
+            break
+    SYMBOLS = yes_no('Do you want symbols in it? [Y]Yes [N]No ')
+    NUMBERS = yes_no('Do you want numbers in it? [Y]Yes [N]No ')
+    LOWER = yes_no('Do you want lower case characters in the password? [Y]Yes [N]No ')
+    UPPER = yes_no('Do you want upper case characters in the password? [Y]Yes [N]No ')
+    print(password.redirect_choice(LENGHT, SYMBOLS, LOWER, UPPER, NUMBERS))
